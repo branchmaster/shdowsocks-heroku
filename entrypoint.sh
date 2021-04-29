5e9d8acc99248ab817ca36a4201d89e97ff6c61d
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-04-29 20:41:36 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /ss
-wget -O ss.tar.xz https://github.com/shadowsocks/shadowsocks-rust/releases/download/v$SS-VER/shadowsocks-v$SS-VER.x86_64-unknown-linux-gnu.tar.xz
-wget -O xp.tar.xz https://github.com/teddysun/xray-plugin/releases/download/v$Xray-plugin-VER/xray-plugin-linux-amd64-v$Xray-plugin-VER.tar.gz
+wget -O ss.tar.xz https://github.com/shadowsocks/shadowsocks-rust/releases/download/v$SSVER/shadowsocks-v$SSVER.x86_64-unknown-linux-gnu.tar.xz
+wget -O xp.tar.xz https://github.com/teddysun/xray-plugin/releases/download/v$XraypluginVER/xray-plugin-linux-amd64-v$XraypluginVER.tar.gz
 tar -zxvf ss.tar.xz /ss/ssserver
 tar -zxvf xp.tar.xz /xp/xray-plugin_linux_amd64
 mv xray-plugin_linux_amd64 xray-plugin
